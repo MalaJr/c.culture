ddc190804bdf7a2987610cde70a38cb48e2b60f8
MalaJr <devallan777@gmail.com>
2025-09-29 03:10:43 -0300

    front-end praticamente filalizado

--- cannabisculture/urls.py
+++ cannabisculture/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import subscribe
+from .views import subscribe, salvar_email
+from . import views
 
 urlpatterns = [
     path('', subscribe, name='subscribe'),
+    path("salvar-email/", views.salvar_email, name="salvar_email"),
 ]
--- cannabisculture/views.py
+++ cannabisculture/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .mongo import emails_collection
 
 def subscribe(request):
     return render(request, 'subscribe.html')
+
+def salvar_email(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+
+        if not email:
+            return JsonResponse({"status": "erro", "mensagem": "Nenhum e-mail recebido"})
+
+        # Verifica se já existe no banco
+        existente = emails_collection.find_one({"email": email})
+
+        if existente:
+            return JsonResponse({"status": "erro", "mensagem": "Este e-mail já está cadastrado"})
+
+        # Insere novo e-mail
+        emails_collection.insert_one({"email": email})
+        return JsonResponse({"status": "ok", "mensagem": "E-mail salvo com sucesso!"})
+
+    return JsonResponse({"status": "erro", "mensagem": "Método inválido"})
